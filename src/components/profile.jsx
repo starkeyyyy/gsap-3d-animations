26317242e89055eed9df0f5bb7bdc57cb17780c8
starkey <tanishqgupta013@gmail.com>
2025-05-12 13:17:26 +0530

    minor fixes

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -20,6 +20,9 @@ const Scene = () => {
   return (
     <div className="main-container">
       <div className="hide"></div>
+      <div className="hide"></div>
+      <div className="hide"></div>
+      <div className="hide"></div>
       <Canvas
         style={{ width: "100vw", height: "100vh" }}
         onMouseMove={handleMouseMove}
@@ -40,67 +43,3 @@ const Scene = () => {
 
 export default Scene;
 
-// import { Canvas, useFrame } from '@react-three/fiber'
-// import React, { useRef } from 'react'
-// import * as THREE from 'three'
-// import "./profile.css"
-// import { Model } from '../assets/walle.jsx'
-
-// // function FollowCube({ mouse }) {
-// //   const cubeRef = useRef()
-
-// //   // useFrame(() => {
-// //   //   if (cubeRef.current) {
-// //   //     const maxTilt = THREE.MathUtils.degToRad(30) // 15 degree tilt limit
-
-// //   //     // Calculate desired rotation based on mouse
-// //   //     const targetRotX = THREE.MathUtils.clamp(mouse.y * maxTilt, -maxTilt, maxTilt)
-// //   //     const targetRotY = THREE.MathUtils.clamp(mouse.x * maxTilt, -maxTilt, maxTilt)
-
-// //   //     // Smooth Rotation
-// //   //     cubeRef.current.rotation.x = THREE.MathUtils.lerp(
-// //   //       cubeRef.current.rotation.x,
-// //   //       targetRotX,
-// //   //       0.1
-// //   //     )
-// //   //     cubeRef.current.rotation.y = THREE.MathUtils.lerp(
-// //   //       cubeRef.current.rotation.y,
-// //   //       targetRotY,
-// //   //       0.1
-// //   //     )
-// //   //   }
-// //   // })
-
-// //   return (
-// //     <mesh ref={cubeRef} position={[0, 0, 0]}>
-// //       <boxGeometry args={[1, 1, 1]} />
-// //       {/* Always pink front face */}
-// //       {Array(6).fill().map((_, idx) => (
-// //         <meshStandardMaterial
-// //           attach={`material-${idx}`}
-// //           key={idx}
-// //           color={idx === 4 ? 'hotpink' : 'orange'} // 4 => Front Face
-// //         />
-// //       ))}
-// //     </mesh>
-// //   )
-// // }
-
-// export default function App() {
-//   const mouse = useRef({ x: 0, y: 0 })
-
-//   const handleMouseMove = (event) => {
-//     const { innerWidth, innerHeight } = window
-//     mouse.current.x = (event.clientX / innerWidth) * 2 - 1
-//     mouse.current.y = ((event.clientY / innerHeight) * 2 - 1)
-//   }
-
-//   return (
-//     <Canvas onMouseMove={handleMouseMove}>
-//       <ambientLight />
-//       <directionalLight position={[5, 5, 5]} />
-//       {/* <FollowCube mouse={mouse.current} /> */}
-//       <Model mouse = {mouse.current}/>
-//     </Canvas>
-//   )
-// }
--- src/components/text.jsx
+++ src/components/text.jsx
@@ -1,81 +1,82 @@
 import { Scroll } from "@react-three/drei";
 import React from "react";
-import "./styles/SceneText.css"
+import "./styles/SceneText.css";
 
 const SceneText = () => {
   return (
     <Scroll html>
+      
       <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
 
-    <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+      <div className="scroll-text">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
 
-    <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+      <div className="scroll-text">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
 
-    <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+      <div className="scroll-text">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
 
-    <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+      <div className="scroll-text">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
 
-    <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+      <div className="scroll-text">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
 
-    <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+      <div className="scroll-text">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
 
-    <div className="scroll-text">
-      <h1 >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
-        beatae tempore quibusdam adipisci corporis ratione mollitia nulla
-        deserunt, quo debitis hic neque totam suscipit quasi velit fuga
-        delectus. Tempora, laboriosam quasi.
-      </h1>
-    </div>
+      <div className="scroll-text">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
+          beatae tempore quibusdam adipisci corporis ratione mollitia nulla
+          deserunt, quo debitis hic neque totam suscipit quasi velit fuga
+          delectus. Tempora, laboriosam quasi.
+        </h1>
+      </div>
     </Scroll>
   );
 };
